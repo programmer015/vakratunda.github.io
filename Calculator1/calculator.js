c73707714af83216276ef26144dd36ccc7af230c
programmer015 <114351766+programmer015@users.noreply.github.com>
2023-09-08 21:34:04 +0530

    Update calculator.js

--- Calculator1/calculator.js
+++ Calculator1/calculator.js
@@ -1,14 +1,43 @@
-let string =""
-let buttons = document.querySelectorAll('button');
+var string =""
+let buttons = document.querySelectorAll('.btn');
+// const button = document.querySelector('.historyButton');
+let history = document.getElementById('history');
+let para = document.createElement('p');
+let todoDiv = document.createElement("div");
+let todoItem = document.createElement("li");
+// let square = <sup>2</sup>;
+// button.addEventListener('click', hist)
 Array.from(buttons).forEach((buttons)=>{
     buttons.addEventListener('click', (e)=>{
     
     if (e.target.innerHTML == '=') {
+        var text = document.createElement("p");
+        text.innerText=string+'='+eval(string);
+        document.getElementById('history').appendChild(text);
         string = eval(string);
         document.querySelector('input').value = string;
+        // para.innerText = string;
+        // history.appendChild(para);
+        // e.preventDefault();
+        // console.log(input.value);
+       
+        // document.querySelector('.history').innerHTML = string;
     }
     else if (e.target.innerHTML == 'AC') {
         string = ""
+        document.querySelector('input').value = string;
+    }
+    else if (e.target.innerHTML == "x<sup>2</sup>") {
+        string = eval(string+"*"+string);
+        document.querySelector('input').value = string;
+        console.log(string);
+    }
+    else if (e.target.innerHTML == "CE") {
+        string = string.substring(0, string.length-1);
+        document.querySelector('input').value = string;
+    }
+    else if (e.target.innerHTML == "e") {
+        string = "2.7182";
         document.querySelector('input').value = string;
     }
     else {
@@ -16,10 +45,53 @@ Array.from(buttons).forEach((buttons)=>{
         string = string + e.target.innerHTML;
         document.querySelector('input').value = string;
     }
-    
+
+    return string;
 })
 })
 
+// s
+
+function myFunction() {
+    var checkBox = document.getElementById("first");
+    var text = document.getElementById("history");
+    if (checkBox.checked == false){
+      text.style.display = "none";
+    } else {
+       text.style.display = "block";
+    }
+  }
+
+// function other () {
+//     document.getElementById('e').innerText="e";
+// }
+
+// function hist(event)
+// {
+//     event.preventDefault();
+//     // console.log(input.value);
+//     const todoDiv = document.createElement("div")
+//     todoDiv.classList.add("hisdiv")
+
+//     const todoItem = document.createElement("li")
+//     todoItem.classList.add("hisList")
+//     todoItem.innerText= string;
+//     todoDiv.appendChild(todoItem)
+
+//     // const deleteButton = document.createElement("button")
+//     // deleteButton.classList.add("delete-btn")
+//     // deleteButton.innerHTML = `<i class="fa-solid fa-trash-can"></i>`
+//     // todoDiv.appendChild(deleteButton)
+
+//     // const completeButton = document.createElement("button")
+//     // completeButton.classList.add("complete-btn")
+//     // completeButton.innerHTML = `<i class="fa-solid fa-check"></i>`
+//     // todoDiv.appendChild(completeButton)
+
+//     // todoList.appendChild(todoDiv)
+//     // input.value = "";
+
+// }
 
 
 
